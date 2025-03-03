dd1f5e143bec90d93b17b9b94ee8d641fbd796ea
MajinDohko <agarridoluengo@gmail.com>
2025-03-03 18:07:55 +0100

    ejercicio completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
 import './App.css'
+import { useState } from 'react'
+// import ReactDom from 'react-dom/client';
 
 function App() {
  //Aquí tu código
+  const [name, setName] = useState('Sofia')
+  const [newName, setNewName] = useState('')
+
+    const changeName =() => {
+        event.preventDefault();
+        if (newName!== "") {
+          setName(newName);
+          setNewName("");
+        }
+    }
+    return (
+        <div>
+            <h2>Teacher Name: {name}</h2>
+            <form onSubmit={changeName}>
+        <input
+          type="text"
+          value={newName}
+          onChange={(name) => setNewName(name.target.value)}
+          placeholder="add a name"
+        />
+        <button type="submit">Add</button>
+      </form>
+        </div>
+    )
 }
 
-export default App
+//   return (
+//     <div>
+//         <h2>Teacher Name: {name}</h2>
+//         <ul>
+//             <li onClick={()=>setName("Data")}>Data</li>
+//             <li onClick={()=>setName("Reyes")}>Reyes</li>
+//             <li onClick={()=>setName("Yolanda")}>Yolanda</li>
+//         </ul>
+//     </div>
+//   )
+// }
+
+export default App;
+
